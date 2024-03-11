c055043df5ab235b963491f1b7698b3b9e578b5a
Rohit grover <129241282+rohitgrove@users.noreply.github.com>
2024-03-11 14:46:10 +0530

    SecondApproachOfPeakIndex

--- SearchingAndSorting/Lec-1/FindPeakElement.java
+++ SearchingAndSorting/Lec-1/FindPeakElement.java
@@ -21,8 +21,32 @@ public class FindPeakElement {
         return s;
     }
 
+    public static int peakIndexInMountainArray(int[] nums) {
+        int ans = -1;
+        int start = 0;
+        int end = nums.length- 1;
+
+        while(start <= end){
+            int mid = (start + end) / 2;
+            if(nums[mid + 1] > nums[mid]){
+                start = mid + 1;
+            }
+            else if(nums[mid - 1] > nums[mid]){
+                end = mid - 1;
+            }
+            else {
+                // It must be Peak
+                ans = mid;
+                break;
+            }
+        }
+        return ans;
+    }
+
     public static void main(String[] args) {
         int nums[] = { 1, 2, 3, 1 };
-        System.out.println(findPeakElement(nums));
+        System.out.println(peakIndexInMountainArray(nums));
+        int nums2[] = { 10, 20, 50, 40, 30 };
+        System.out.println(peakIndexInMountainArray(nums2));
     }
 }
