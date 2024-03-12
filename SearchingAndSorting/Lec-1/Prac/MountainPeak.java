1433843368be48cb07ca13f6e9d5478f939edff2
Rohit grover <129241282+rohitgrove@users.noreply.github.com>
2024-03-12 18:25:37 +0530

    Practice

--- SearchingAndSorting/Lec-1/Prac/MountainPeak.java
+++ SearchingAndSorting/Lec-1/Prac/MountainPeak.java
@@ -14,10 +14,29 @@ public class MountainPeak {
         return s;
     }
 
+    public static int peak2(int nums[]) {
+        int s = 0;
+        int e = nums.length - 1;
+        int ans = -1;
+        while (s <= e) {
+            int mid = s + (e - s) / 2;
+            if (nums[mid] < nums[mid + 1]) {
+                s = mid + 1;
+            } else if (nums[mid] < nums[mid - 1]) {
+                e = mid - 1;
+            } else {
+                ans = mid;
+                break;
+            }
+        }
+
+        return ans;
+    }
+
     public static void main(String[] args) {
         int nums1[] = { 1, 2, 3, 1 };
-        System.out.println(peak(nums1));
+        System.out.println(peak2(nums1));
         int nums2[] = { 10, 20, 50, 40, 30 };
-        System.out.println(peak(nums2));
+        System.out.println(peak2(nums2));
     }
 }
