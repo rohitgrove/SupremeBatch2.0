e1fdf3b8fa27607d2ec0cba1648abe75375b68de
Rohit grover <129241282+rohitgrove@users.noreply.github.com>
2024-03-16 17:14:16 +0530

    Prac

--- Arrays/Prac/MaximumAvgSubArray.java
+++ Arrays/Prac/MaximumAvgSubArray.java
@@ -2,16 +2,17 @@ public class MaximumAvgSubArray {
     public static double findMaxSubAvg(int nums[], int k) {
         return slidingWindow(nums, k);
     }
+
     public static double bruteForce(int nums[], int k) {
         int i = 0, j = k - 1;
         int maxSum = Integer.MIN_VALUE;
         while (j < nums.length) {
             int sum = 0;
-            for (int y = i; y <= j; y++) {
+            for (int y = i; y <= j; ++y) {
                 sum += nums[y];
             }
-            j++;
-            i++;
+            ++i;
+            ++j;
 
             maxSum = Math.max(maxSum, sum);
         }
@@ -21,23 +22,25 @@ public class MaximumAvgSubArray {
     }
 
     public static double slidingWindow(int nums[], int k) {
-        int i = 0, j = k-1, sum = 0;
+        int i = 0, j = k - 1;
+        int sum = 0;
         for (int y = i; y <= j; y++) {
             sum += nums[y];
         }
 
         int maxSum = sum;
+
         j++;
         while (j < nums.length) {
             sum -= nums[i++];
             sum += nums[j++];
-
             maxSum = Math.max(maxSum, sum);
         }
 
-        double ans = maxSum / (double) k;
-        return ans;
+        double maxAvg = maxSum / (double) k;
+        return maxAvg;
     }
+
     public static void main(String[] args) {
         int nums[] = { 1, 12, -5, -6, 50, 3 }, k = 4;
         System.out.println(findMaxSubAvg(nums, k));
