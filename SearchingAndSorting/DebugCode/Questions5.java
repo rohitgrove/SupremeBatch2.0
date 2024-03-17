8f36265be5c81e3bdbb66194819a19866b6618a2
Rohit grover <129241282+rohitgrove@users.noreply.github.com>
2024-03-17 10:21:53 +0530

    Update Question 5

--- SearchingAndSorting/DebugCode/Questions5.java
+++ SearchingAndSorting/DebugCode/Questions5.java
@@ -1,26 +1,27 @@
 import java.util.ArrayList;
+import java.util.List;
 
 public class Questions5 {
     public int lengthOfLIS(int[] nums) {
-        ArrayList<Integer> ans = new ArrayList<>();
+        List<Integer> ans = new ArrayList<>();
         ans.add(nums[0]);
-        
+
         for (int i = 1; i < nums.length; i++) {
-            if (nums[i] < ans.get(ans.size() - 1)) {
+            if (nums[i] > ans.get(ans.size() - 1)) {
                 ans.add(nums[i]);
             } else {
                 int index = binarySearch(ans, nums[i]);
                 ans.set(index, nums[i]);
             }
         }
-        
+
         return ans.size();
     }
 
-    private int binarySearch(ArrayList<Integer> nums, int target) {
+    private int binarySearch(List<Integer> nums, int target) {
         int left = 0;
         int right = nums.size() - 1;
-        
+
         while (left < right) {
             int mid = left + (right - left) / 2;
             if (nums.get(mid) < target) {
@@ -29,12 +30,11 @@ public class Questions5 {
                 right = mid;
             }
         }
-        
-        // Adjusting the return value if target is greater than all numbers in the list
+
         if (left == nums.size() - 1 && nums.get(left) < target) {
             left++;
         }
-        
+
         return left;
     }
 
