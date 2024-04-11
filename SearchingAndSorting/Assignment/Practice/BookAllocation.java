6195c2ff0bf13d4caed9c250b7352bec3fda818f
Rohit grover <groverrohit54@gmail.com>
2024-04-11 13:31:21 +0530

    Prac

--- SearchingAndSorting/Assignment/Practice/BookAllocation.java
+++ SearchingAndSorting/Assignment/Practice/BookAllocation.java
@@ -1,19 +1,23 @@
 public class BookAllocation {
-    public static int findPages(int arr[], int N, int M) {
-        int sum = 0, s = 0;
-        for (int i = 0; i < arr.length; i++) {
-            sum += arr[i];
+    public static int findPages(int[] arr, int N, int M) {
+        if (M > N) {
+            return -1;
         }
-        int e = sum;
 
-        int mid = s + (e - s) / 2;
+        int s = 0;
+        int e = 0;
+        for (int i = 0; i < arr.length; i++) {
+            e += arr[i];
+        }
+
         int ans = -1;
+        int mid = s + (e - s) / 2;
         while (s <= e) {
-            if (isPossible(arr, N, M, mid)) {
+            if (isPossible(arr, M, mid, N)) {
                 ans = mid;
                 e = mid - 1;
             } else {
-                s = mid + 1;
+                s = mid + 1; 
             }
             mid = s + (e - s) / 2;
         }
@@ -21,18 +25,18 @@ public class BookAllocation {
         return ans;
     }
 
-    public static boolean isPossible(int arr[], int N, int M, int sol) {
-        int sumOfPages = 0;
+    public static boolean isPossible(int[] arr, int M, int mid, int N) {
         int student = 1;
-        for (int i = 0; i < N; i++) {
-            if (sumOfPages + arr[i] <= sol) {
-                sumOfPages += arr[i];
+        int sumOfPage = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (sumOfPage + arr[i] <= mid) {
+                sumOfPage += arr[i];
             } else {
                 student++;
-                if (student > M || arr[i] > sol) {
+                if (student > M || arr[i] >= mid) {
                     return false;
                 }
-                sumOfPages = arr[i];
+                sumOfPage = arr[i];
             }
         }
 
@@ -42,5 +46,5 @@ public class BookAllocation {
     public static void main(String[] args) {
         int N = 4, arr[] = { 12, 34, 67, 90 }, M = 2;
         System.out.println(findPages(arr, N, M));
-    }    
+    }
 }
