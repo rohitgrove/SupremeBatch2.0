1d3c998a5f4f15fd992b6ac54f0561a995cfc11a
Rohit grover <groverrohit54@gmail.com>
2024-06-02 09:14:59 +0530

    Module2

---
+++ OOPs/CollectionFrameWork/Module-2/ArrayDequeBasics.java
@@ -0,0 +1,24 @@
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class ArrayDequeBasics {
+    public static void main(String[] args) {
+        Deque<Integer> q = new ArrayDeque<>();
+        q.offer(10);
+        q.offerFirst(5);
+        q.offerLast(50);
+        System.out.println(q);
+
+        q.pollLast();
+        System.out.println(q);
+        q.pollFirst();
+        System.out.println(q);
+
+        System.out.println(q.size());
+
+        System.out.println(q.peek());
+        System.out.println(q.peekFirst());
+        System.out.println(q.peekLast());
+
+    }
+}
