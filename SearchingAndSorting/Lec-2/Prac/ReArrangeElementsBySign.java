bc519f87ac059a3b5cb589d215240ae67967c873
Rohit grover <129241282+rohitgrove@users.noreply.github.com>
2024-07-02 17:49:24 +0530

    Practice

---
+++ SearchingAndSorting/Lec-2/Prac/ReArrangeElementsBySign.java
@@ -0,0 +1,34 @@
+public class ReArrangeElementsBySign {
+    public static int[] rearrangeArray(int[] nums) {
+        int ans[] = new int[nums.length];
+        int even = 0, odd = 1;
+        for (int i = 0; i < nums.length; i++) {
+            int num = nums[i];
+            if (num > 0) {
+                ans[even] = num;
+                even += 2;
+            } else {
+                ans[odd] = num;
+                odd += 2;
+            }
+        }
+
+        return ans;
+    }
+    
+    public static void printArr(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int nums1[] = { 3, 1, -2, -5, 2, -4 };
+        printArr(nums1);
+        printArr(rearrangeArray(nums1));
+        int nums2[] = { -1, 1 };
+        printArr(nums2);
+        printArr(rearrangeArray(nums2));
+    } 
+}
