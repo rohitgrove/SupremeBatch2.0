4d6b61ccdf3f51ca1fc8f9c64a0fa18c0f0ba89e
Rohit grover <129241282+rohitgrove@users.noreply.github.com>
2024-07-16 12:26:25 +0530

    Lec-3

---
+++ Stacks/Lec-3/NextSmaller.java
@@ -0,0 +1,68 @@
+import java.util.Stack;
+
+public class NextSmaller {
+    public static int[] nextSmallerRight(int arr[]) {
+        Stack<Integer> st = new Stack<>();
+        st.push(-1);
+        int ans[] = new int[arr.length];
+
+        for (int i = arr.length - 1; i >= 0; i--) {
+            int curr = arr[i];
+            // anser find karo curr k liye
+            while (!st.isEmpty() && st.peek() >= curr) {
+                st.pop();
+            }
+
+            ans[i] = st.peek();
+
+            st.push(curr);
+        }
+
+        return ans;
+    }
+
+    public static int[] prevSmaller(int arr[]) {
+        Stack<Integer> st = new Stack<>();
+        st.push(-1);
+        int ans[] = new int[arr.length];
+
+        for (int i = 0; i < arr.length; i++) {
+            int curr = arr[i];
+            // anser find karo curr k liye
+            while (!st.isEmpty() && st.peek() >= curr) {
+                st.pop();
+            }
+
+            ans[i] = st.peek();
+
+            st.push(curr);
+        }
+
+        return ans;
+    }
+
+    public static void printArr(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int[] arr1 = { 8, 4, 6, 2, 3 };
+        int[] arr2 = { 4, 1, 6, 3 };
+        int[] arr3 = { 2, 6, 8, 9, 3 };
+        System.out.println("Original Arr");
+        printArr(arr1);
+        printArr(arr2);
+        printArr(arr3);
+        System.out.println("Next Smaller Right");
+        printArr(nextSmallerRight(arr1));
+        printArr(nextSmallerRight(arr2));
+        printArr(nextSmallerRight(arr3));
+        System.out.println("Prev Smaller");
+        printArr(prevSmaller(arr1));
+        printArr(prevSmaller(arr2));
+        printArr(prevSmaller(arr3));
+    }
+}
