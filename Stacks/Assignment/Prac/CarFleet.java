a0b54e18a418d2dc1666493868a7ecda0e14a149
Rohit grover <129241282+rohitgrove@users.noreply.github.com>
2024-07-30 12:31:39 +0530

    CarFleet1

--- Stacks/Assignment/Prac/CarFleet.java
+++ Stacks/Assignment/Prac/CarFleet.java
@@ -6,8 +6,7 @@ import java.util.Stack;
 public class CarFleet {
     static class Car {
         int position, speed;
-
-        public Car(int position, int speed) {
+        public Car( int position, int speed) {
             this.position = position;
             this.speed = speed;
         }
@@ -26,8 +25,9 @@ public class CarFleet {
             Car car = new Car(position[i], speed[i]);
             cars.add(car);
         }
-        Collections.sort(cars, new CustomComparator());
 
+        Collections.sort(cars, new CustomComparator());
+    
         Stack<Float> st = new Stack<>();
         for (int i = 0; i < cars.size(); i++) {
             float time = (target - cars.get(i).position) / ((float) cars.get(i).speed);
@@ -36,13 +36,14 @@ public class CarFleet {
             }
             st.push(time);
         }
+
         return st.size();
-    }
+    }  
 
     public static void main(String[] args) {
         int target1 = 12, position1[] = { 10, 8, 0, 5, 3 }, speed1[] = { 2, 4, 1, 1, 3 };
         System.out.println(carFleet(target1, position1, speed1));
         int target2 = 100, position2[] = { 0, 2, 4 }, speed2[] = { 4, 2, 1 };
         System.out.println(carFleet(target2, position2, speed2));
-    }
+    } 
 }
