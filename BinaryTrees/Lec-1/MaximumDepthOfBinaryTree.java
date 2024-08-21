9aa09311d0de0d078923fad211fc5bd25f0c529d
Rohit grover <129241282+rohitgrove@users.noreply.github.com>
2024-08-21 10:58:15 +0530

    MaximumDepthOfBinaryTree

---
+++ BinaryTrees/Lec-1/MaximumDepthOfBinaryTree.java
@@ -0,0 +1,66 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class MaximumDepthOfBinaryTree {
+    public static int maxDepthNode(Node root) {
+        if (root == null) {
+            return 0;
+        }
+
+        int leftHeight = maxDepthNode(root.left);
+        int rightHeight = maxDepthNode(root.right);
+        int height = Math.max(leftHeight, rightHeight) + 1;
+        return height;
+    }
+
+    public static int maxDepthEdge(Node root) {
+        if (root == null) {
+            return -1;
+        }
+
+        int leftHeight = maxDepthEdge(root.left);
+        int rightHeight = maxDepthEdge(root.right);
+        int height = Math.max(leftHeight, rightHeight) + 1;
+        return height;
+    }
+
+    public static int maxDepthLevelOrder(Node root) {
+        if (root == null) {
+            return 0;
+        }
+        Queue<Node> q = new LinkedList<>();
+        q.add(root);
+        q.add(null);
+        int height = 1;
+
+        while (!q.isEmpty()) {
+            Node front = q.poll();
+
+            if (front == null) {
+                System.out.println();
+                if (!q.isEmpty()) {
+                    q.add(null);
+                    height++;
+                }
+            } else {
+                if (front.left != null) {
+                    q.add(front.left);
+                }
+                if (front.right != null) {
+                    q.add(front.right);
+                }
+            }
+        }
+
+        return height;
+    }
+
+    public static void main(String[] args) {
+        int data1[] = { 3, 9, -1, -1, 20, 15, -1, -1, 7, -1, -1 };
+        Node root1 = ConstructTree.createTree(data1);
+        System.out.println(maxDepthNode(root1));
+        int data2[] = { 10, 20, 40, -1, -1, 50, 70, -1, -1, 80, -1, -1, 30, -1, 60, -1, -1 };
+        Node root2 = ConstructTree.createTree(data2);
+        System.out.println(maxDepthNode(root2));
+    }
+}
