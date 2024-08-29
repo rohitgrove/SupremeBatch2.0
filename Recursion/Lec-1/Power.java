e379800d6979756ae97fb32e05a981c20a40b358
Rohit grover <129241282+rohitgrove@users.noreply.github.com>
2024-08-29 09:49:51 +0530

    Optimized aproach

--- Recursion/Lec-1/Power.java
+++ Recursion/Lec-1/Power.java
@@ -10,12 +10,32 @@ public class Power {
     }
 
     public static int pow(int a, int b) {
+        if (b == 0) {
+            return 1;
+        }
+
         if (b == 1) {
             return a;
         }
 
         int ans = a * pow(a, b - 1);
         return ans;
+    }
+
+    public static int optimizedPower(int a, int n) { // O(logn)
+        if (n == 0) {
+            return 1;
+        }
+
+        int halfPower = optimizedPower(a, n / 2);
+        // System.out.println(halfPower);
+        int halfPowerSq = halfPower * halfPower;
+
+        // n is odd
+        if (n % 2 != 0) {
+            halfPowerSq = a * halfPowerSq;
+        }
+        return halfPowerSq;
     }
 
     public static void main(String[] args) {
