29d7d6b7789dddfafb765e1a95af54a7c5d4455a
Rohit grover <129241282+rohitgrove@users.noreply.github.com>
2024-08-31 19:52:33 +0530

    MapsLec-1

---
+++ Maps/Lec-1/CountCharacters.java
@@ -0,0 +1,23 @@
+import java.util.Map;
+import java.util.HashMap;
+import java.util.TreeMap;
+
+public class CountCharacters {
+    public static void countCharacters(HashMap<Character, Integer> mapping, String str) {
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            mapping.put(ch, mapping.getOrDefault(ch, 0) + 1);
+        }
+    }
+
+    public static void main(String[] args) {
+        String str = "lovebbabbar";
+        HashMap<Character, Integer> mapping = new HashMap<>(); // unsorted map
+        // TreeMap<Character, Integer> sortedMap = new TreeMap<>(); // sorted map
+        countCharacters(mapping, str);
+
+        for (Map.Entry<Character, Integer> entry : mapping.entrySet()) {
+            System.out.println(entry.getKey() + " -> " + entry.getValue());
+        }
+    }
+}
