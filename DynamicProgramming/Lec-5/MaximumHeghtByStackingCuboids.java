e861280181f285be57d326aabd9422a4e31a83a4
Rohit grover <129241282+rohitgrove@users.noreply.github.com>
2024-09-12 17:27:13 +0530

    formatting

--- DynamicProgramming/Lec-5/MaximumHeghtByStackingCuboids.java
+++ DynamicProgramming/Lec-5/MaximumHeghtByStackingCuboids.java
@@ -46,8 +46,10 @@ public class MaximumHeghtByStackingCuboids {
         }
         // Sort cuboids to process them in a sorted order
         Arrays.sort(cuboids, (a, b) -> {
-            if (a[0] != b[0]) return a[0] - b[0];
-            if (a[1] != b[1]) return a[1] - b[1];
+            if (a[0] != b[0])
+                return a[0] - b[0];
+            if (a[1] != b[1])
+                return a[1] - b[1];
             return a[2] - b[2];
         });
 
