15695fb44900e53c4a27436a297a8355987d92ea
Rohit grover <129241282+rohitgrove@users.noreply.github.com>
2024-09-19 15:48:35 +0530

    HouseRobber3

--- DynamicProgramming/Assignment/UniqueBST2.java
+++ DynamicProgramming/Assignment/UniqueBST2.java
@@ -1,27 +1,7 @@
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-
-class TreeNode {
-    int val;
-    TreeNode left;
-    TreeNode right;
-
-    TreeNode() {
-    }
-
-    TreeNode(int val) {
-        this.val = val;
-    }
-
-    TreeNode(int val, TreeNode left, TreeNode right) {
-        this.val = val;
-        this.left = left;
-        this.right = right;
-    }
-}
 
 public class UniqueBST2 {
     public static List<TreeNode> allPossibleBSTsUsingRec(int start, int end) {
