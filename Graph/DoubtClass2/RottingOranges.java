65a50e134b7c5bbbe0859a0b719ee4befd6e3cdb
Rohit grover <129241282+rohitgrove@users.noreply.github.com>
2024-10-05 06:08:26 +0530

    AddComment

--- Graph/DoubtClass2/RottingOranges.java
+++ Graph/DoubtClass2/RottingOranges.java
@@ -1,7 +1,7 @@
 public class RottingOranges {
     public static void dfs(int[][] grid, int i, int j, int min) {
         if (i < 0 || j < 0 || i >= grid.length || j >= grid[0].length || grid[i][j] == 0
-                || (grid[i][j] < min && grid[i][j] > 1)) {
+                || (grid[i][j] < min && grid[i][j] > 1 /* if node already visited */)) {
             return;
         }
 
