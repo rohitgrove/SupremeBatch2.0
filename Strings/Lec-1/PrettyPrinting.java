841faa7cc158f5239544bf7ce329abc9568885d6
Rohit grover <129241282+rohitgrove@users.noreply.github.com>
2024-10-16 08:17:50 +0530

    Formating

--- Strings/Lec-1/PrettyPrinting.java
+++ Strings/Lec-1/PrettyPrinting.java
@@ -2,6 +2,6 @@ public class PrettyPrinting {
     public static void main(String[] args) {
         float a = 453.1274f;
         // System.out.printf("Formatted Number is %.2f",a);
-        System.out.printf("Pie: %.3f",Math.PI);
+        System.out.printf("Pie: %.3f", Math.PI);
     }
 }
