da71642cb3f73fe9596a3da033712c153f1d391c
Rohit grover <129241282+rohitgrove@users.noreply.github.com>
2024-10-18 22:32:05 +0530

    RomanToInteger

--- Strings/Assignment/RomanToInteger.java
+++ Strings/Assignment/RomanToInteger.java
@@ -5,7 +5,7 @@
 // C             100
 // D             500
 // M             1000
-public class IntegerToRoman {
+public class RomanToInteger {
     public static String intToRoman(int num) {
         String[] romanSymbols = { "M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I" };
         int values[] = { 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1 };
