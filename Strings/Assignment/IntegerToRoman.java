14aeb5fc9b8bcdc0937b6961588af2127d02c9f1
Rohit grover <129241282+rohitgrove@users.noreply.github.com>
2024-10-18 22:40:50 +0530

    IntegerToRoman

--- Strings/Assignment/IntegerToRoman.java
+++ Strings/Assignment/IntegerToRoman.java
@@ -5,7 +5,7 @@
 // C             100
 // D             500
 // M             1000
-public class RomanToInteger {
+public class IntegerToRoman {
     public static String intToRoman(int num) {
         String[] romanSymbols = { "M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I" };
         int values[] = { 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1 };
@@ -21,11 +21,13 @@ public class RomanToInteger {
     }
 
     public static void main(String[] args) {
-        int num1 = 3;
+        int num1 = 3749;
         System.out.println(intToRoman(num1));
         int num2 = 58;
         System.out.println(intToRoman(num2));
         int num3 = 1994;
         System.out.println(intToRoman(num3));
+        int num4 = 3;
+        System.out.println(intToRoman(num4));
     }
 }
