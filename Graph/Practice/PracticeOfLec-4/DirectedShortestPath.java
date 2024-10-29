ab47852aeb2d52393b82aeaa33ac68b793406106
Rohit grover <129241282+rohitgrove@users.noreply.github.com>
2024-10-29 15:29:45 +0530

    Practice

--- Graph/Practice/PracticeOfLec-4/DirectedShortestPath.java
+++ Graph/Practice/PracticeOfLec-4/DirectedShortestPath.java
@@ -6,34 +6,36 @@ import java.util.Arrays;
 public class DirectedShortestPath {
     public static HashMap<Integer, List<Pair>> adj = new HashMap<>();
 
-    public static void topoOrderDFSUtil(int src, Stack<Integer> st, HashMap<Integer, Boolean> visited) {
+    public static void topoOrderDFSUtil(int src, HashMap<Integer, Boolean> visited, Stack<Integer> st) {
         visited.put(src, true);
 
         if (adj.containsKey(src)) {
             for (Pair nbr : adj.get(src)) {
                 int nbrNode = nbr.node;
-                visited.put(nbrNode, true);
-                topoOrderDFSUtil(nbrNode, st, visited);
+                if (!visited.containsKey(nbrNode)) {
+                    topoOrderDFSUtil(nbrNode, visited, st);
+                }
             }
         }
 
-        st.add(src);
+        st.push(src);
     }
 
     public static void topoOrderDFS(int n) {
         int src = 0;
         Stack<Integer> st = new Stack<>();
         HashMap<Integer, Boolean> visited = new HashMap<>();
-        topoOrderDFSUtil(src, st, visited);
+        topoOrderDFSUtil(src, visited, st);
 
-        shortestPath(st, n);
+        shortestPath(n, st);
     }
 
-    public static void shortestPath(Stack<Integer> st, int n) {
-        int dist[] = new int[n];
-        Arrays.fill(dist, Integer.MAX_VALUE);
+    public static void shortestPath(int n, Stack<Integer> st) {
+        int dest[] = new int[n];
+        Arrays.fill(dest, Integer.MAX_VALUE);
+
         int src = st.pop();
-        dist[src] = 0;
+        dest[src] = 0;
 
         if (!adj.containsKey(src)) {
             return;
@@ -42,28 +44,28 @@ public class DirectedShortestPath {
         for (Pair nbr : adj.get(src)) {
             int nbrNode = nbr.node;
             int nbrDist = nbr.dist;
-            if (dist[src] + nbrDist < dist[nbrNode]) {
-                dist[nbrNode] = dist[src] + nbrDist;
-            }
+            if (nbrDist + dest[src] < dest[nbrNode]) {
+                dest[nbrNode] = nbrDist + dest[src];
+            }   
         }
 
         while (!st.isEmpty()) {
-            src = st.pop();
-
-            if (!adj.containsKey(src)) {
+            int frontNode = st.pop();
+            
+            if (!adj.containsKey(frontNode)) {
                 continue;
             }
 
-            for (Pair nbr : adj.get(src)) {
+            for (Pair nbr : adj.get(frontNode)) {
                 int nbrNode = nbr.node;
                 int nbrDist = nbr.dist;
-                if (dist[src] + nbrDist < dist[nbrNode]) {
-                    dist[nbrNode] = dist[src] + nbrDist;
+                if (nbrDist + dest[frontNode] < dest[nbrNode]) {
+                    dest[nbrNode] = dest[frontNode] + nbrDist;
                 }
             }
         }
 
-        for (int i : dist) {
+        for (int i : dest) {
             System.out.print(i + " ");
         }
     }
