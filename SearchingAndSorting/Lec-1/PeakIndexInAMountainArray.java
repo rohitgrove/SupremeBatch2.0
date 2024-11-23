7cd24814fdbe0b1584e65f4e24760e09d7ddfde1
Rohit grover <129241282+rohitgrove@users.noreply.github.com>
2024-11-23 13:40:19 +0530

    AddExample

--- SearchingAndSorting/Lec-1/PeakIndexInAMountainArray.java
+++ SearchingAndSorting/Lec-1/PeakIndexInAMountainArray.java
@@ -1,18 +1,18 @@
 public class PeakIndexInAMountainArray {
-    public static int peakIndexInMountainArray(int[] arr) {
+    public static int peakIndexInMountainArray(int[] nums) {
         int s = 0;
-        int e = arr.length -1 ;
-        
+        int e = nums.length - 1;
+
         while (s < e) {
             int mid = s + (e - s) / 2;
-            if (arr[mid] < arr[mid + 1]) {
+            if (nums[mid] < nums[mid + 1]) {
                 // A wali me hu
                 // peak right me exist karti h
                 s = mid + 1;
             } else {
                 // yaa toh B line pr hu
                 // ya toh main peak element pr hu
-                e = mid; 
+                e = mid;
             }
         }
 
@@ -20,7 +20,9 @@ public class PeakIndexInAMountainArray {
     }
 
     public static void main(String[] args) {
-        int nums[] = { 10, 20, 50, 40, 30 };
-        System.out.println(peakIndexInMountainArray(nums));
+        int nums1[] = { 10, 20, 50, 40, 30 };
+        System.out.println(peakIndexInMountainArray(nums1));
+        int nums2[] = { 10, 20, 30, 90, 70, 60, 50, 40 };
+        System.out.println(peakIndexInMountainArray(nums2));
     }
 }
