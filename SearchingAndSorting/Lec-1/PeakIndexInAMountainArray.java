d7cfbc62ab14e04589bf50744233f32ad55cafc1
Rohit grover <129241282+rohitgrove@users.noreply.github.com>
2024-11-24 13:06:51 +0530

    OneMoreSolution

--- SearchingAndSorting/Lec-1/PeakIndexInAMountainArray.java
+++ SearchingAndSorting/Lec-1/PeakIndexInAMountainArray.java
@@ -1,5 +1,5 @@
 public class PeakIndexInAMountainArray {
-    public static int peakIndexInMountainArray(int[] nums) {
+    public static int peakIndexInMountainArray1(int[] nums) {
         int s = 0;
         int e = nums.length - 1;
 
@@ -19,10 +19,31 @@ public class PeakIndexInAMountainArray {
         return s;
     }
 
+    public static int peakIndexInMountainArray2(int[] nums) {
+        int s = 0;
+        int e = nums.length - 1;
+        int ans = -1;
+
+        while (s <= e) {
+            int mid = s + (e - s) / 2;
+            if (nums[mid] < nums[mid + 1]) {
+                s = mid + 1;
+            } else if (nums[mid] < nums[mid - 1]) {
+                e = mid - 1;
+            } else {
+                // It must be Peak
+                ans = mid;
+                break;
+            }
+        }
+
+        return ans;
+    }
+
     public static void main(String[] args) {
         int nums1[] = { 10, 20, 50, 40, 30 };
-        System.out.println(peakIndexInMountainArray(nums1));
+        System.out.println(peakIndexInMountainArray2(nums1));
         int nums2[] = { 10, 20, 30, 90, 70, 60, 50, 40 };
-        System.out.println(peakIndexInMountainArray(nums2));
+        System.out.println(peakIndexInMountainArray2(nums2));
     }
 }
