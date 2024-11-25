298395f30fe761a0ec2f216280ccdf81d252e036
Rohit grover <129241282+rohitgrove@users.noreply.github.com>
2024-11-25 11:22:13 +0530

    OneMoreSolution

--- SearchingAndSorting/Lec-2/Sqrtx.java
+++ SearchingAndSorting/Lec-2/Sqrtx.java
@@ -1,5 +1,5 @@
 public class Sqrtx {
-    public static int mySqrt(int x) {
+    public static int mySqrt1(int x) {
         int s = 0;
         int e = x;
         long mid = s + (e - s) / 2;
@@ -23,8 +23,29 @@ public class Sqrtx {
         return ans;
     }
 
+    public static double mySqrt2(int number) {
+        double low = 0;
+        double high = number;
+        double mid = 0;
+
+        // Precision up to 3 decimal places
+        double precision = 0.001;
+
+        while ((high - low) > precision) {
+            mid = (low + high) / 2;
+
+            if (mid * mid > number) {
+                high = mid;
+            } else {
+                low = mid;
+            }
+        }
+
+        return Math.round(mid * 1000.0) / 1000.0;
+    }
+
     public static void main(String[] args) {
-        System.out.println(mySqrt(4));
-        System.out.println(mySqrt(68));
+        System.out.println(mySqrt2(4));
+        System.out.println(mySqrt2(68));
     }
 }
