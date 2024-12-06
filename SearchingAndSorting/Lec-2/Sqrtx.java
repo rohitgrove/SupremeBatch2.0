339f58a64acfa359d9e30a22c66a5c9fb69a6f4f
Rohit grover <129241282+rohitgrove@users.noreply.github.com>
2024-12-06 08:45:03 +0530

    Update Sqrtx.java

--- SearchingAndSorting/Lec-2/Sqrtx.java
+++ SearchingAndSorting/Lec-2/Sqrtx.java
@@ -1,5 +1,5 @@
 public class Sqrtx {
-    public static int mySqrt1(int x) {
+    public static int mySqrt(int x) {
         int s = 0;
         int e = x;
         long mid = s + (e - s) / 2;
@@ -23,41 +23,8 @@ public class Sqrtx {
         return ans;
     }
 
-    public static double mySqrt2(int x) {
-        int s = 0;
-        int e = x;
-        long mid = s + (e - s) / 2;
-        double ans = -1.0;
-
-        while (s <= e) {
-            // kya mid hi toh answer nahi
-            if (mid * mid == x) {
-                return (double) mid;
-            } else if (mid * mid < x) {
-                // ans store
-                // right me jao
-                ans = (double) mid;
-                s = (int) mid + 1;
-            } else {
-                e = (int) mid - 1;
-            }
-            mid = s + (e - s) / 2;
-        }
-
-        double incr = 0.1;
-        while (incr > 0.001) {
-            while (ans * ans <= x) {
-                ans += incr;
-            }
-            ans -= incr;
-            incr /= 10;
-        }
-
-        return ans;
-    }
-
     public static void main(String[] args) {
-        System.out.println(mySqrt2(4));
-        System.out.println(mySqrt2(68));
+        System.out.println(mySqrt(4));
+        System.out.println(mySqrt(68));
     }
 }
