415513babaf2163bdcc066a387f4bf5c72bf1401
Rohit grover <129241282+rohitgrove@users.noreply.github.com>
2024-12-09 12:26:52 +0530

    AddOneMoreSollution

--- Strings/DoubtClass/RemoveAllOccurrencesOfASubString.java
+++ Strings/DoubtClass/RemoveAllOccurrencesOfASubString.java
@@ -1,5 +1,5 @@
 public class RemoveAllOccurrencesOfASubString {
-    public static String removeOccurrences(String s, String part) {
+    public static String removeOccurrences1(String s, String part) {
         while (s.contains(part)) {
             int found = s.indexOf(part);
             String lp = s.substring(0, found);
@@ -11,10 +11,28 @@ public class RemoveAllOccurrencesOfASubString {
         return s;
     }
 
+    public static String removeOccurrences2(String s, String part) {
+        while (true) {
+            int found = s.indexOf(part);
+            if (found == -1) {
+                break;
+            }
+
+            // remove karna hai
+            String LP = s.substring(0, found);
+            String RP = s.substring(found + part.length());
+
+            // concat bath Parts
+            s = LP + RP;
+        }
+
+        return s;
+    }
+
     public static void main(String[] args) {
         String s1 = "daabcbaabcbc", part1 = "abc";
-        System.out.println(removeOccurrences(s1, part1));
+        System.out.println(removeOccurrences1(s1, part1));
         String s2 = "axxxxyyyyb", part2 = "xy";
-        System.out.println(removeOccurrences(s2, part2));
+        System.out.println(removeOccurrences1(s2, part2));
     }
 }
