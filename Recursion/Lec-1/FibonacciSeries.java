f43d60429b5e4bd7c82d35fa36ca66b1e93d39e3
Rohit grover <129241282+rohitgrove@users.noreply.github.com>
2024-12-24 09:25:02 +0530

    CombinedBaseCondition

--- Recursion/Lec-1/FibonacciSeries.java
+++ Recursion/Lec-1/FibonacciSeries.java
@@ -1,11 +1,8 @@
 public class FibonacciSeries {
     public static int fib(int n) {
         // base case
-        if (n == 0) {
-            return 0;
-        }
-        if (n == 1) {
-            return 1;
+        if (n == 0 || n == 1) {
+            return n;
         }
 
         return fib(n - 1) + fib(n - 2);
