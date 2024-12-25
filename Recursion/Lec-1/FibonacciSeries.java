6134771f02c7e46d09aa8d8a3dc48f4aa436c8c3
Rohit grover <129241282+rohitgrove@users.noreply.github.com>
2024-12-25 11:03:21 +0530

    Lec-1

--- Recursion/Lec-1/FibonacciSeries.java
+++ Recursion/Lec-1/FibonacciSeries.java
@@ -8,6 +8,22 @@ public class FibonacciSeries {
         return fib(n - 1) + fib(n - 2);
     }
 
+    public static int fibUsingLoop(int n) {
+        if (n == 1) {
+            return 1;
+        }
+        int prev1 = 0;
+        int prev2 = 1;
+        int ans = 0;
+        for (int i = 1; i < n; i++) {
+            ans = prev1 + prev2;
+            prev1 = prev2;
+            prev2 = ans;
+        }
+
+        return ans;
+    }
+
     public static void main(String[] args) {
         System.out.println(fib(7));
     }
