f99f1bb97d3661821c47169fa1dc3ab943d4cea7
Rohit grover <129241282+rohitgrove@users.noreply.github.com>
2024-12-25 12:59:34 +0530

    Lec-2

--- Recursion/Lec-2/SearchInArray.java
+++ Recursion/Lec-2/SearchInArray.java
@@ -1,5 +1,5 @@
 public class SearchInArray {
-    public static boolean search(int arr[], int size, int idx, int target) {
+    public static boolean search1(int arr[], int size, int idx, int target) {
         // base case
         if (idx >= size) {
             return false;
@@ -10,16 +10,38 @@ public class SearchInArray {
         }
 
         // recursive call
-        return search(arr, size, idx + 1, target);
+        return search1(arr, size, idx + 1, target);
+    }
+
+    public static int search2(int arr[], int size, int idx, int target) {
+        if (idx >= size) {
+            return -1;
+        }
+
+        if (arr[idx] == target) {
+            return idx;
+        }
+
+        return search2(arr, size, idx + 1, target);
     }
 
     public static void main(String[] args) {
         int arr[] = { 10, 20, 30, 40, 50 };
-        boolean ans = search(arr, arr.length, 0, 50);
-        if (ans) {
-            System.out.println("element is present");
+        int index = 0;
+        int target = 50;
+
+        // boolean ans = search1(arr, arr.length, index, target);
+        // if (ans) {
+        //     System.out.println("element is present");
+        // } else {
+        //     System.out.println("element is not present");
+        // }
+
+        int ans = search2(arr, target, index, target);
+        if (ans != -1) {
+            System.out.println("Element Found At: " + ans);
         } else {
-            System.out.println("element is not present");
+            System.out.println("Element Not Found");
         }
     }
 }
