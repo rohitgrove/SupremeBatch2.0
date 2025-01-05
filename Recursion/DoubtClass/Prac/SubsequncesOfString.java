ac27a483baf7aaf765fab3a1c7f1e2b9b2d49eda
Rohit grover <129241282+rohitgrove@users.noreply.github.com>
2025-01-05 11:42:22 +0530

    DoubtClass

---
+++ Recursion/DoubtClass/Prac/SubsequncesOfString.java
@@ -0,0 +1,25 @@
+public class SubsequncesOfString {
+    public static void subsequncesOfString(String s) {
+        int num_sub = (int) Math.pow(2, s.length());
+
+        for (int n = 0; n < num_sub; n++) {
+            String out = "";
+            int num = n;
+            int i = 0;
+            while (num > 0) {
+                int b = (num & 1);
+                if (b == 1) {
+                    out += s.charAt(i);
+                }
+                i++;
+                num >>= 1;
+            }
+            System.out.println(out);
+        }
+    }
+
+    public static void main(String[] args) {
+        String str = "abc";
+        subsequncesOfString(str);
+    }
+}
