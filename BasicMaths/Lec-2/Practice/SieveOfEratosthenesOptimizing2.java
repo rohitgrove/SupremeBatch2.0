42e1b53205e0abce430b41e88ee8993e5b022713
Rohit grover <129241282+rohitgrove@users.noreply.github.com>
2025-01-23 16:05:17 +0530

    Lec-2

---
+++ BasicMaths/Lec-2/Practice/SieveOfEratosthenesOptimizing2.java
@@ -0,0 +1,36 @@
+
+import java.util.Arrays;
+
+public class SieveOfEratosthenesOptimizing2 {
+    public static int sieveOfEratosthenes(int n) {
+        if (n <= 1) {
+            return 0;
+        }
+
+        boolean isPrime[] = new boolean[n];
+        Arrays.fill(isPrime, true);
+        isPrime[0] = isPrime[1] = false;
+
+        for (int i = 2; i * i < n; i++) {
+            if (isPrime[i]) {
+                int j = i * i;
+                while (j < n) {
+                    isPrime[j] = false;
+                    j += i;
+                }
+            }
+        }
+
+        int cnt = 0;
+        for (int i = 0; i < isPrime.length; i++) {
+            if (isPrime[i]) {
+                cnt++;
+            }
+        }
+        return cnt;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(sieveOfEratosthenes(10));
+    }
+}
