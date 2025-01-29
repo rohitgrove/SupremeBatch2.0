c5ffd51aa6621051c2c589c68e45647ffc6ec6c8
Rohit grover <129241282+rohitgrove@users.noreply.github.com>
2025-01-29 16:26:50 +0530

    3Questions

---
+++ LinkedList/Practice/Assignment/DoubleANumber.java
@@ -0,0 +1,41 @@
+public class DoubleANumber {
+    static int carry = 0;
+
+    public static void solve(Node head) {
+        if (head == null) {
+            return;
+        }
+
+        solve(head.next);
+
+        int sum = (head.data * 2) + carry;
+        head.data = sum % 10;
+        carry = sum / 10;
+    }
+
+    public static Node doubleIt(Node head) {
+        solve(head);
+        if (carry != 0) {
+            Node newNode = new Node(carry);
+            newNode.next = head;
+            head = newNode;
+        }
+        return head;
+    }
+
+    public static void main(String[] args) {
+        LinkedList ll1 = new LinkedList();
+        ll1.insertAtTail(1);
+        ll1.insertAtTail(8);
+        ll1.insertAtTail(9);
+        ll1.printLL();
+        ll1.printLL(doubleIt(ll1.head));
+
+        LinkedList ll2 = new LinkedList();
+        ll2.insertAtTail(9);
+        ll2.insertAtTail(9);
+        ll2.insertAtTail(9);
+        ll2.printLL();
+        ll2.printLL(doubleIt(ll2.head));
+    }
+}
---
+++ LinkedList/Practice/Assignment/MergeNodesBetweenZeroes.java
@@ -0,0 +1,50 @@
+public class MergeNodesBetweenZeroes {
+    public static Node mergeNodes(Node head) {
+        if (head == null) {
+            return head;
+        }
+
+        Node slow = head, fast = head.next, newLastNode = head;
+        int sum = 0;
+        while (fast != null) {
+            if (fast.data != 0) {
+                sum += fast.data;
+            } else {
+                slow.data = sum;
+                newLastNode = slow;
+                slow = slow.next;
+                sum = 0;
+            }
+            fast = fast.next;
+        }
+
+        newLastNode.next = null;
+        return head;
+    }
+
+    public static void main(String[] args) {
+        LinkedList ll1 = new LinkedList();
+        ll1.insertAtTail(0);
+        ll1.insertAtTail(3);
+        ll1.insertAtTail(1);
+        ll1.insertAtTail(0);
+        ll1.insertAtTail(4);
+        ll1.insertAtTail(5);
+        ll1.insertAtTail(2);
+        ll1.insertAtTail(0);
+        ll1.printLL();
+        ll1.printLL(mergeNodes(ll1.head));
+
+        LinkedList ll2 = new LinkedList();
+        ll2.insertAtTail(0);
+        ll2.insertAtTail(1);
+        ll2.insertAtTail(0);
+        ll2.insertAtTail(3);
+        ll2.insertAtTail(0);
+        ll2.insertAtTail(2);
+        ll2.insertAtTail(2);
+        ll2.insertAtTail(0);
+        ll2.printLL();
+        ll2.printLL(mergeNodes(ll2.head));
+    }
+}
