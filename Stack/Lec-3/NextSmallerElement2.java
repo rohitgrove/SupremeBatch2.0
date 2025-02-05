7a5aadcd5e0b0871860f72af218c2c929b48844b
Rohit grover <129241282+rohitgrove@users.noreply.github.com>
2025-02-05 07:52:05 +0530

    StackLec-3

--- Stack/Lec-3/NextSmallerElement2.java
+++ Stack/Lec-3/NextSmallerElement2.java
@@ -1,10 +1,11 @@
 import java.util.Stack;
 
-public class PreviousSmallestElement2 {
-    public static int[] prevSmallerElement(int[] arr) {
+public class NextSmallerElement2 {
+    public static int[] nextSmallerElement(int[] arr) {
         Stack<Integer> st = new Stack<>();
         int ans[] = new int[arr.length];
-        for (int i = arr.length - 1; i >= 0; i--) {
+
+        for (int i = 0; i < arr.length; i++) {
             while (!st.isEmpty() && arr[i] < arr[st.peek()]) {
                 int adult = st.pop();
                 ans[adult] = arr[i];
@@ -28,16 +29,16 @@ public class PreviousSmallestElement2 {
 
     public static void main(String[] args) {
         int arr1[] = { 8, 4, 6, 2, 3 };
-        printArr(prevSmallerElement(arr1));
+        printArr(nextSmallerElement(arr1));
         int arr2[] = { 2, 1, 4, 6, 3 };
-        printArr(prevSmallerElement(arr2));
+        printArr(nextSmallerElement(arr2));
         int arr3[] = { 2, 1, 4, 3 };
-        printArr(prevSmallerElement(arr3));
+        printArr(nextSmallerElement(arr3));
         int arr4[] = { 1, 3, 2 };
-        printArr(prevSmallerElement(arr4));
+        printArr(nextSmallerElement(arr4));
         int arr5[] = { 1, 2, 3, 4 };
-        printArr(prevSmallerElement(arr5));
+        printArr(nextSmallerElement(arr5));
         int arr6[] = { 2, 6, 8, 9, 3 };
-        printArr(prevSmallerElement(arr6));
+        printArr(nextSmallerElement(arr6));
     }
 }
--- Stack/Lec-3/PreviousSmallerElement2.java
+++ Stack/Lec-3/PreviousSmallerElement2.java
@@ -1,11 +1,10 @@
 import java.util.Stack;
 
-public class NextSmallestElement2 {
-    public static int[] nextSmallerElement(int[] arr) {
+public class PreviousSmallerElement2 {
+    public static int[] prevSmallerElement(int[] arr) {
         Stack<Integer> st = new Stack<>();
         int ans[] = new int[arr.length];
-
-        for (int i = 0; i < arr.length; i++) {
+        for (int i = arr.length - 1; i >= 0; i--) {
             while (!st.isEmpty() && arr[i] < arr[st.peek()]) {
                 int adult = st.pop();
                 ans[adult] = arr[i];
@@ -29,16 +28,16 @@ public class NextSmallestElement2 {
 
     public static void main(String[] args) {
         int arr1[] = { 8, 4, 6, 2, 3 };
-        printArr(nextSmallerElement(arr1));
+        printArr(prevSmallerElement(arr1));
         int arr2[] = { 2, 1, 4, 6, 3 };
-        printArr(nextSmallerElement(arr2));
+        printArr(prevSmallerElement(arr2));
         int arr3[] = { 2, 1, 4, 3 };
-        printArr(nextSmallerElement(arr3));
+        printArr(prevSmallerElement(arr3));
         int arr4[] = { 1, 3, 2 };
-        printArr(nextSmallerElement(arr4));
+        printArr(prevSmallerElement(arr4));
         int arr5[] = { 1, 2, 3, 4 };
-        printArr(nextSmallerElement(arr5));
+        printArr(prevSmallerElement(arr5));
         int arr6[] = { 2, 6, 8, 9, 3 };
-        printArr(nextSmallerElement(arr6));
+        printArr(prevSmallerElement(arr6));
     }
 }
