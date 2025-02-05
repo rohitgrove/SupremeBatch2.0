484fff85ee5a184757e7cb836c65213a27a16b43
Rohit grover <129241282+rohitgrove@users.noreply.github.com>
2025-02-05 13:32:46 +0530

    NStacksPractice

---
+++ Stack/Assignment/Prac/NStacks/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        NStack s = new NStack(3, 6);
+        System.out.println(s.push(1, 1));
+        System.out.println(s.push(10, 1));
+        System.out.println(s.push(10, 1));
+        System.out.println(s.push(14, 2));
+        System.out.println(s.push(15, 3));
+        System.out.println(s.push(15, 3));
+        System.out.println(s.push(15, 3));
+        System.out.println(s.pop(1));
+        System.out.println(s.pop(2));
+    }
+}
