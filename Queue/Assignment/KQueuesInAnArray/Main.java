9ff216870859850c1207eb10fc475d779a84cd1d
Rohit grover <129241282+rohitgrove@users.noreply.github.com>
2025-02-13 16:09:01 +0530

    AssignmentQuestions

--- Queue/Assignment/KQueuesInAnArray/Main.java
+++ Queue/Assignment/KQueuesInAnArray/Main.java
@@ -1,12 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        KQueue q = new KQueue(3, 6);
-        System.out.println(q.push(1, 1));
-        System.out.println(q.push(10, 1));
-        System.out.println(q.push(10, 1));
-        System.out.println(q.push(14, 2));
-        System.out.println(q.push(15, 3));
+        KQueue q = new KQueue(8, 3);
+        System.out.println(q.push(1, 0));
+        System.out.println(q.push(2, 0));
+        System.out.println(q.push(5, 1));
+        System.out.println(q.push(3, 0));
         System.out.println(q.pop(1));
-        System.out.println(q.pop(2));
+        System.out.println(q.pop(1));
+        System.out.println(q.pop(0));
+        System.out.println(q.pop(0));
+        System.out.println(q.pop(0));
+        System.out.println(q.pop(0));
+        System.out.println(q.pop(0));
+        System.out.println(q.pop(1));
     }
 }
