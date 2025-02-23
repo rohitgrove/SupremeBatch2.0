b48fca91eda5d064a25f146e0fb5a31bf437e625
Rohit grover <129241282+rohitgrove@users.noreply.github.com>
2025-02-23 19:39:04 +0530

    AssignmentCompleted

--- BinaryTrees/Assignments/FindDuplicateSubtrees.java
+++ BinaryTrees/Assignments/FindDuplicateSubtrees.java
@@ -3,25 +3,34 @@ import java.util.HashMap;
 import java.util.List;
 
 public class FindDuplicateSubtrees {
+    static List<Node> ans = new ArrayList<>();
+    static HashMap<String, Integer> subTreeMap = new HashMap<>();
+
     public static List<Node> findDuplicateSubtrees(Node root) {
-        List<Node> res = new ArrayList<>();
-        HashMap<String, Integer> hm = new HashMap<>();
-        helper(res, root, hm);
-        return res;
+        preOrder(root);
+        return ans;
     }
 
-    public static String helper(List<Node> res, Node root, HashMap<String, Integer> hm) {
-        if (root == null)
-            return "";
-        String left = helper(res, root.left, hm);
-        String right = helper(res, root.right, hm);
-        int currroot = root.data;
-        String stringformed = currroot + "$" + left + "$" + right;
-        if (hm.getOrDefault(stringformed, 0) == 1) {
-            res.add(root);
+    public static String preOrder(Node root) {
+        if (root == null) {
+            return "N";
         }
-        hm.put(stringformed, hm.getOrDefault(stringformed, 0) + 1);
-        return stringformed;
+
+        String curr = Integer.toString(root.data);
+        String l = preOrder(root.left);
+        String r = preOrder(root.right);
+
+        String s = curr + "," + l + "," + r;
+        if (subTreeMap.containsKey(s)) {
+            if (subTreeMap.get(s) == 1) {
+                ans.add(root);
+            }
+            subTreeMap.put(s, subTreeMap.get(s) + 1);
+        } else {
+            subTreeMap.put(s, 1);
+        }
+
+        return s;
     }
 
     public static void main(String[] args) {
