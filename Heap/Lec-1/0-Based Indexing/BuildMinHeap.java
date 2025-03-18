8f6df78aa72ae43885faa724109ec8b17807e871
Rohit grover <129241282+rohitgrove@users.noreply.github.com>
2025-03-18 10:34:58 +0530

    HeapLec-1

--- Heap/Lec-1/0-Based Indexing/BuildMinHeap.java
+++ Heap/Lec-1/0-Based Indexing/BuildMinHeap.java
@@ -31,7 +31,7 @@ public class BuildMinHeap {
     }
 
     public static void printArr(int arr[]) {
-        for (int i = 1; i < arr.length; i++) {
+        for (int i = 0; i < arr.length; i++) {
             System.out.print(arr[i] + " ");
         }
         System.out.println();
