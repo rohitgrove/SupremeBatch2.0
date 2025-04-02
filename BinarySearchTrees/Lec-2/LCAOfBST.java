a21334f89913146b85b44a2d69aa36b67f12f501
Rohit grover <129241282+rohitgrove@users.noreply.github.com>
2025-04-02 07:32:46 +0530

    BSTLec-2

--- BinarySearchTrees/Lec-2/LCAOfBST.java
+++ BinarySearchTrees/Lec-2/LCAOfBST.java
@@ -1,5 +1,5 @@
 public class LCAOfBST {
-    public static Node lowestCommonAncestor(Node root, Node p, Node q) {
+    public static Node lowestCommonAncestor1(Node root, Node p, Node q) {
         // base case
         if (root == null) {
             return null;
@@ -7,14 +7,16 @@ public class LCAOfBST {
 
         // case 1: p and q both comes in left subtree of root node
         if (p.data < root.data && q.data < root.data) {
-            Node leftAns = lowestCommonAncestor(root.left, p, q);
+            Node leftAns = lowestCommonAncestor1(root.left, p, q);
             return leftAns;
         }
+
         // case 2: p and q both comes in right subtree of root node
         if (p.data > root.data && q.data > root.data) {
-            Node rightAns = lowestCommonAncestor(root.right, p, q);
+            Node rightAns = lowestCommonAncestor1(root.right, p, q);
             return rightAns;
         }
+
         // case 3: p is in left subtree of root and q is in right subtree of root node
         if (p.data < root.data && q.data > root.data) {
             return root;
@@ -28,14 +30,37 @@ public class LCAOfBST {
         return root;
     }
 
+    public static Node lowestCommonAncestor2(Node root, Node p, Node q) {
+        // base case
+        if (root == null) {
+            return null;
+        }
+
+        // case 1: p and q both comes in left subtree of root node
+        if (p.data < root.data && q.data < root.data) {
+            Node leftAns = lowestCommonAncestor2(root.left, p, q);
+            return leftAns;
+        }
+        // case 2: p and q both comes in right subtree of root node
+        if (p.data > root.data && q.data > root.data) {
+            Node rightAns = lowestCommonAncestor2(root.right, p, q);
+            return rightAns;
+        }
+
+        return root;
+    }
+
     public static void main(String[] args) {
         int data1[] = { 30, 50, 70, 100, 120, 200, 300 };
         Node root1 = ConstructTree.bstFromInorder(data1, 0, data1.length - 1);
-        System.out.println(lowestCommonAncestor(root1, root1.left.left, root1.left.right).data);
+        System.out.println(lowestCommonAncestor1(root1, root1.left.left, root1.left.right).data);
+        System.out.println(lowestCommonAncestor2(root1, root1.left.left, root1.left.right).data);
 
         int data2[] = { 6, 2, 8, 0, 4, 7, 9, 3, 5 };
         Node root2 = ConstructTree.createTree(data2);
-        System.out.println(lowestCommonAncestor(root2, root2.left, root2.right).data);
-        System.out.println(lowestCommonAncestor(root2, root2.left, root2.left.right).data);
+        System.out.println(lowestCommonAncestor1(root2, root2.left, root2.right).data);
+        System.out.println(lowestCommonAncestor2(root2, root2.left, root2.right).data);
+        System.out.println(lowestCommonAncestor1(root2, root2.left, root2.left.right).data);
+        System.out.println(lowestCommonAncestor2(root2, root2.left, root2.left.right).data);
     }
 }
