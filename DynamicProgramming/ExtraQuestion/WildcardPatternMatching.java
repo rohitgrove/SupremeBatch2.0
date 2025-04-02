3ae7fbb46048cd676ffcde7fb943fcceb7beb462
Rohit grover <129241282+rohitgrove@users.noreply.github.com>
2025-04-02 12:18:02 +0530

    WildcardPatternMatching

--- DynamicProgramming/ExtraQuestion/WildcardPatternMatching.java
+++ DynamicProgramming/ExtraQuestion/WildcardPatternMatching.java
@@ -95,6 +95,42 @@ public class WildcardPatternMatching {
         return dp[s.length()][p.length()] == 1;
     }
 
+    public static boolean solveTabSO(String s, String p) {
+        int prev[] = new int[p.length() + 1];
+        int curr[] = new int[p.length() + 1];
+
+        prev[0] = 1;
+        for (int idx2 = 1; idx2 <= p.length(); idx2++) {
+            boolean flag = true;
+            for (int k = 1; k <= idx2; k++) {
+                if (p.charAt(k - 1) != '*') {
+                    flag = false;
+                    break;
+                }
+            }
+            prev[idx2] = flag ? 1 : 0;
+        }
+
+        for (int idx1 = 1; idx1 <= s.length(); idx1++) {
+            for (int idx2 = 1; idx2 <= p.length(); idx2++) {
+                boolean result;
+                if (s.charAt(idx1 - 1) == p.charAt(idx2 - 1) || p.charAt(idx2 - 1) == '?') {
+                    result = prev[idx2 - 1] == 1;
+                } else if (p.charAt(idx2 - 1) == '*') {
+                    result = curr[idx2 - 1] == 1 || prev[idx2] == 1;
+                } else {
+                    result = false;
+                }
+
+                curr[idx2] = result ? 1 : 0;
+            }
+
+            prev = curr.clone();
+        }
+
+        return prev[p.length()] == 1;
+    }
+
     public static boolean isMatch(String s, String p) {
         // return solve(s, p, s.length() - 1, p.length() - 1);
         // int[][] dp = new int[s.length()][p.length()];
@@ -102,7 +138,7 @@ public class WildcardPatternMatching {
         // Arrays.fill(row, -1);
         // }
         // return solveMemo(s, p, s.length() - 1, p.length() - 1, dp);
-        return solveTab(s, p);
+        return solveTabSO(s, p);
     }
 
     public static void main(String[] args) {
