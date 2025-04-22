59036c9ff2c7c3db7a6d7581f8ea5c6b012040ef
Rohit grover <129241282+rohitgrove@users.noreply.github.com>
2025-04-22 16:40:03 +0530

    GraphLec-3

---
+++ Graph/Practice/PracticeOfLec-3/Graph.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class Graph {
+    public void addEdge(int u, int v, boolean direction, HashMap<Integer, List<Integer>> adjList) {
+        adjList.putIfAbsent(u, new ArrayList<>());
+        if (direction) {
+            adjList.get(u).add(v);
+        } else {
+            adjList.putIfAbsent(v, new ArrayList<>());
+            adjList.get(u).add(v);
+            adjList.get(v).add(u);
+        }
+    }
+
+    public void printAdjList(HashMap<Integer, List<Integer>> adjList) {
+        for (int src : adjList.keySet()) {
+            System.out.print(src + "-> {");
+            for (int neighbour : adjList.get(src)) {
+                System.out.print(neighbour + ", ");
+            }
+            System.out.println("}");
+        }
+        System.out.println();
+    }
+}
---
+++ Graph/Practice/PracticeOfLec-3/ShortestPath.java
@@ -0,0 +1,58 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class ShortestPath {
+    public static HashMap<Integer, List<Integer>> adjList = new HashMap<>();
+
+    public static void shortestPath(int src, int dest) {
+        Queue<Integer> q = new LinkedList<>();
+        HashMap<Integer, Boolean> vis = new HashMap<>();
+        HashMap<Integer, Integer> parent = new HashMap<>();
+
+        q.offer(src);
+        vis.put(src, true);
+        parent.put(src, -1);
+
+        while (!q.isEmpty()) {
+            int frontNode = q.poll();
+
+            for (int nbr : adjList.get(frontNode)) {
+                if (!vis.containsKey(nbr)) {
+                    q.offer(nbr);
+                    vis.put(nbr, true);
+                    parent.put(nbr, frontNode);
+                }
+            }
+        }
+
+        List<Integer> ans = new ArrayList<>();
+        while (dest != -1) {
+            ans.add(dest);
+            dest = parent.get(dest);
+        }
+
+        Collections.reverse(ans);
+        System.out.println(ans);
+    }
+
+    public static void main(String[] args) {
+        Graph g = new Graph();
+        g.addEdge(0, 4, false, adjList);
+        g.addEdge(4, 1, false, adjList);
+        g.addEdge(1, 5, false, adjList);
+
+        g.addEdge(0, 3, false, adjList);
+        g.addEdge(3, 5, false, adjList);
+
+        g.addEdge(0, 6, false, adjList);
+        g.addEdge(6, 2, false, adjList);
+        g.addEdge(2, 5, false, adjList);
+
+        g.printAdjList(adjList);
+        shortestPath(0, 5);
+    }
+}
