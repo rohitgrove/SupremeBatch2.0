4dde09e3e4612e77ddb7f49b0c7a082a6b7d345c
Rohit grover <129241282+rohitgrove@users.noreply.github.com>
2025-08-02 17:06:16 +0530

    Lec-3

--- SearchingAndSorting/Lec-3/FindTheOddOccuringElements.java
+++ SearchingAndSorting/Lec-3/FindTheOddOccuringElements.java
@@ -11,7 +11,7 @@ public class FindTheOddOccuringElements {
             }
 
             // mid check -> even or odd
-            if ((mid&1) == 1) { // (mid&1) == 1 -> true-> odd number
+            if ((mid & 1) == 1) { // (mid&1) == 1 -> true-> odd number
                 if (mid - 1 >= 0 && nums[mid] == nums[mid - 1]) {
                     // right me jao
                     s = mid + 1;
