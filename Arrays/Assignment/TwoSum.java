aa5c5a1ba29689ad6151cdf21f18674a47d5de31
Rohit grover <129241282+rohitgrove@users.noreply.github.com>
2025-09-08 07:41:05 +0530

    Arrays

--- Arrays/Assignment/TwoSum.java
+++ Arrays/Assignment/TwoSum.java
@@ -1,7 +1,7 @@
 import java.util.Arrays;
 
 public class TwoSum {
-    public static boolean hasArrayTwoCandidates1(int arr[], int n, int x) {
+    public static boolean twoSum(int arr[], int n, int x) {
         for (int i = 0; i < n; i++) {
             for (int j = i + 1; j < n; j++) {
                 if (arr[i] + arr[j] == x) {
@@ -13,7 +13,7 @@ public class TwoSum {
         return false;
     }
 
-    public static boolean hasArrayTwoCandidates2(int arr[], int n, int x) {
+    public static boolean twoSum2PointerApproach(int arr[], int n, int x) {
         Arrays.sort(arr);
         int low = 0;
         int high = arr.length - 1;
@@ -31,9 +31,13 @@ public class TwoSum {
         return false;
     }
 
+    public static boolean hasArrayTwoCandidates(int arr[], int n, int x) {
+        return twoSum2PointerApproach(arr, n, x);
+    }
+
     public static void main(String[] args) {
         int Arr[] = { 1, 4, 45, 6, 10, 8 };
         int N = Arr.length, X = 16;
-        System.out.println(hasArrayTwoCandidates2(Arr, N, X));
+        System.out.println(hasArrayTwoCandidates(Arr, N, X));
     }
 }
