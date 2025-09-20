2051cebd63f355f8325df8caeebdf20d83cafba6
Rohit grover <129241282+rohitgrove@users.noreply.github.com>
2025-09-20 08:38:03 +0530

    Lec-3

--- Backtracking&DnC/Lec-3/SudokuSolver/SudokuSolver2.java
+++ Backtracking&DnC/Lec-3/SudokuSolver/SudokuSolver2.java
@@ -1,21 +1,32 @@
 public class SudokuSolver2 {
     public static boolean isSafe(int row, int col, char value, char[][] board) {
+        // same row me vo number na ho
+        // same column me vo number na ho
+        // same 3*3 matrix me no ho
         int n = board.length;
 
+        // row check
         for (int i = 0; i < n; i++) {
-            // row check
             if (board[row][i] == value) {
                 return false;
             }
+        }
 
-            // col check
+        // column check
+        for (int i = 0; i < n; i++) {
             if (board[i][col] == value) {
                 return false;
             }
+        }
 
-            // 3*3 box check
-            if (board[3 * (row / 3) + (i / 3)][3 * (col / 3) + (i % 3)] == value) {
-                return false;
+        int sr = (row / 3) * 3;
+        int sc = (col / 3) * 3;
+        // 3*3 box check karo
+        for (int i = sr; i < sr + 3; i++) {
+            for (int j = sc; j < sc + 3; j++) {
+                if (board[i][j] == value) {
+                    return false;
+                }
             }
         }
 
@@ -24,22 +35,27 @@ public class SudokuSolver2 {
 
     public static boolean solveSudokuHelper(char[][] board) {
         int n = board.length;
-
         for (int r = 0; r < n; r++) {
             for (int c = 0; c < n; c++) {
+                // empty cell check karo
                 if (board[r][c] == '.') {
+                    // try karenge -> 1 -> 9 insert krne ka
                     for (char ch = '1'; ch <= '9'; ch++) {
                         if (isSafe(r, c, ch, board)) {
+                            // place krdo
                             board[r][c] = ch;
-
-                            if (solveSudokuHelper(board)) {
+                            // recursion
+                            boolean recursionKaAnswer = solveSudokuHelper(board);
+                            if (recursionKaAnswer) {
                                 return true;
                             } else {
+                                // recursion ans nhi la paya
+                                // iska mtlb jo ch insert kia, vo galat tha
+                                // iska mtlb backtrack karo, wapas jao
                                 board[r][c] = '.';
                             }
                         }
                     }
-
                     return false;
                 }
             }
@@ -72,10 +88,8 @@ public class SudokuSolver2 {
                 { '.', '6', '.', '.', '.', '.', '2', '8', '.' },
                 { '.', '.', '.', '4', '1', '9', '.', '.', '5' },
                 { '.', '.', '.', '.', '8', '.', '.', '7', '9' } };
-        System.out.println("Before Solving");
         printArr(board);
         solveSudoku(board);
-        System.out.println("After Solving");
         printArr(board);
     }
 }
