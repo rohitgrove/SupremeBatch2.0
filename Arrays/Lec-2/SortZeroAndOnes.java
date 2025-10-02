bfb4195e4338c3a749fce97b6ca39694ac24473c
Rohit grover <129241282+rohitgrove@users.noreply.github.com>
2025-10-02 09:39:17 +0530

    Arrays

--- Arrays/Lec-2/SortZeroAndOnes.java
+++ Arrays/Lec-2/SortZeroAndOnes.java
@@ -1,5 +1,5 @@
 public class SortZeroAndOnes {
-    public static void sortZeroOne(int[] arr, int n) {
+    public static void sortZeroOne1(int[] arr, int n) {
         int zeroCount = 0;
         int oneCount = 0;
 
@@ -32,6 +32,33 @@ public class SortZeroAndOnes {
         }
     }
 
+    public static void sortZeroOne2(int[] arr, int size) {
+        int s = 0;
+        int e = size - 1;
+
+        while (s < e) {
+            // when arr[s]==0 then increase only s++
+            while (s < e && arr[s] == 0) {
+                s++;
+            }
+
+            // when arr[e]==1 then decrease only se--
+            while (s < e && arr[e] == 1) {
+                e--;
+            }
+
+            // when arr[s] == 1 and arr[e] == 0 then
+            // increase and decrease respectively s++, e-- and swap
+            if (s < e && arr[s] == 1 && arr[e] == 0) {
+                int temp = arr[s];
+                arr[s] = arr[e];
+                arr[e] = temp;
+                s++;
+                e--;
+            }
+        }
+    }
+
     public static void printArr(int arr[]) {
         for (int i = 0; i < arr.length; i++) {
             System.out.print(arr[i] + " ");
@@ -42,7 +69,7 @@ public class SortZeroAndOnes {
     public static void main(String[] args) {
         int[] arr = { 0, 1, 0, 1, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0 };
         int n = 14;
-        sortZeroOne(arr, n);
+        sortZeroOne2(arr, n);
         printArr(arr);
     }
 }
