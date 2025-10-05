4f9465a3db6f7c3ceb5d087b47e166dd940ccd48
Rohit grover <129241282+rohitgrove@users.noreply.github.com>
2025-10-05 12:45:25 +0530

    BinaryTreesLec-2

--- BinaryTrees/Lec-2/ConstructTreeUsingPostOrderAndInOrder.java
+++ BinaryTrees/Lec-2/ConstructTreeUsingPostOrderAndInOrder.java
@@ -1,33 +1,6 @@
 import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.Queue;
 
 public class ConstructTreeUsingPostOrderAndInOrder {
-    public static void levelOrder(Node root) {
-        Queue<Node> q = new LinkedList<>();
-        q.offer(root);
-        q.offer(null);
-
-        while (!q.isEmpty()) {
-            Node front = q.poll();
-            if (front == null) {
-                System.out.println();
-                if (!q.isEmpty()) {
-                    q.offer(null);
-                }
-            } else {
-                System.out.print(front.data + " ");
-                if (front.left != null) {
-                    q.offer(front.left);
-                }
-
-                if (front.right != null) {
-                    q.offer(front.right);
-                }
-            }
-        }
-    }
-
     public static void createMapping(int[] postorder, int size, HashMap<Integer, Integer> valueToIndex) {
         for (int i = 0; i < size; i++) {
             int element = postorder[i];
@@ -52,7 +25,6 @@ public class ConstructTreeUsingPostOrderAndInOrder {
         // right ki call phele lagegi
         root.right = constructTreeFromPostAndInorderTraversal(valueToIndexMap, postOrder, inOrder, postIndex, position + 1, inOrderEnd, size);
         root.left = constructTreeFromPostAndInorderTraversal(valueToIndexMap, postOrder, inOrder, postIndex, inOrderStart, position - 1, size);
-
         return root;
     }
 
@@ -71,6 +43,6 @@ public class ConstructTreeUsingPostOrderAndInOrder {
         int inOrder[] = { 8, 14, 6, 2, 10, 4 };
         int postOrder[] = { 8, 6, 14, 4, 10, 2 };
         Node root = buildTree(postOrder, inOrder);
-        levelOrder(root);
+        Traversals.levelOrder(root);
     }
 }
--- BinaryTrees/Lec-2/ConstructTreeUsingPreOrderAndInOrder.java
+++ BinaryTrees/Lec-2/ConstructTreeUsingPreOrderAndInOrder.java
@@ -1,33 +1,6 @@
 import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.Queue;
 
 public class ConstructTreeUsingPreOrderAndInOrder {
-    public static void levelOrder(Node root) {
-        Queue<Node> q = new LinkedList<>();
-        q.offer(root);
-        q.offer(null);
-
-        while (!q.isEmpty()) {
-            Node front = q.poll();
-            if (front == null) {
-                System.out.println();
-                if (!q.isEmpty()) {
-                    q.offer(null);
-                }
-            } else {
-                System.out.print(front.data + " ");
-                if (front.left != null) {
-                    q.offer(front.left);
-                }
-
-                if (front.right != null) {
-                    q.offer(front.right);
-                }
-            }
-        }
-    }
-
     public static int searchInorder(int[] inOrder, int size, int element) {
         for (int i = 0; i < size; i++) {
             if (inOrder[i] == element) {
@@ -83,6 +56,6 @@ public class ConstructTreeUsingPreOrderAndInOrder {
         int inorder[] = { 10, 8, 6, 2, 4, 12 };
         int preOrder[] = { 2, 8, 10, 6, 4, 12 };
         Node root = buildTree(preOrder, inorder);
-        levelOrder(root);
+        Traversals.levelOrder(root);
     }
 }
