b9999b348af8969640ad059c6f5e87dd5b01d065
Rohit grover <129241282+rohitgrove@users.noreply.github.com>
2026-03-19 09:41:12 +0530

    PracticeOfDoubtClass1

---
+++ Graph/Practice/PracticeOfDoubtClass1/NumberOfIslandsWay1.java
@@ -0,0 +1,50 @@
+public class NumberOfIslandsWay1 {
+    public static boolean dfs(char[][] grid, int row, int col) {
+        if (row < 0 || col < 0 || row >= grid.length || col >= grid[0].length || grid[row][col] == '0'
+                || grid[row][col] == 'x') {
+            return false;
+        }
+
+        grid[row][col] = 'x';
+        boolean marked = true;
+
+        boolean up = dfs(grid, row - 1, col);
+        boolean down = dfs(grid, row + 1, col);
+        boolean left = dfs(grid, row, col - 1);
+        boolean right = dfs(grid, row, col + 1);
+
+        boolean ans = up || down || left || right;
+
+        return marked || ans;
+    }
+
+    public static int numIslands(char[][] grid) {
+        int ans = 0;
+
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                ans += dfs(grid, i, j) ? 1 : 0;
+            }
+        }
+
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        char grid1[][] = {
+                { '1', '1', '1', '1', '0' },
+                { '1', '1', '0', '1', '0' },
+                { '1', '1', '0', '0', '0' },
+                { '0', '0', '0', '0', '0' }
+        };
+        System.out.println(numIslands(grid1));
+
+        char grid2[][] = {
+                { '1', '1', '0', '0', '0' },
+                { '1', '1', '0', '0', '0' },
+                { '0', '0', '1', '0', '0' },
+                { '0', '0', '0', '1', '1' }
+        };
+        System.out.println(numIslands(grid2));
+    }
+}
