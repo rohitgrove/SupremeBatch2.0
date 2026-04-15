24b001bee5a185f2bdf42636a15d7bb2ff0c1ed8
Rohit grover <129241282+rohitgrove@users.noreply.github.com>
2026-04-15 13:11:51 +0530

    LRUCache

---
+++ MapsAndTries/Practice/LRUCache/Node.java
@@ -0,0 +1,13 @@
+public class Node {
+    public int key;
+    public int val;
+    public Node next;
+    public Node prev;
+
+    public Node(int key, int val) {
+        this.key = key;
+        this.val = val;
+        this.next = null;
+        this.prev = null;
+    }
+}
