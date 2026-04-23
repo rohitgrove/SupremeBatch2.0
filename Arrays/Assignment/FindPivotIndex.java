d715d242ec48d7b8c2a082b9316208c7eefea29c
Rohit grover <129241282+rohitgrove@users.noreply.github.com>
2026-04-23 17:27:38 +0530

    RecoverAllData

--- Arrays/Assignment/FindPivotIndex.java
+++ Arrays/Assignment/FindPivotIndex.java
@@ -52,7 +52,7 @@ public class FindPivotIndex {
 
     public static void printArr(int nums[]) {
         for (int i = 0; i < nums.length; i++) {
-            System.out.print(nums[i]+" ");
+            System.out.print(nums[i] + " ");
         }
         System.out.println();
     }
