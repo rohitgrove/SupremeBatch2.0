72f926fb4dd6b7a7fc78de66e608cc0c2e1653f5
Rohit grover <129241282+rohitgrove@users.noreply.github.com>
2026-08-08 10:25:47 +0530

    HomeWorkQuestion

---
+++ Arrays/HomeWorkQuestion/FindPivotIndex.java
@@ -0,0 +1,24 @@
+public class FindPivotIndex {
+    public static int pivotIndex(int[] nums) {
+        int rightSum = 0;
+        int leftSum = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            rightSum += nums[i];
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            rightSum -= nums[i];
+            if (rightSum == leftSum) {
+                return i;
+            }
+            leftSum += nums[i];
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int nums[] = { 1, 7, 3, 6, 5, 6 };
+        System.out.println(pivotIndex(nums));
+    }
+}
